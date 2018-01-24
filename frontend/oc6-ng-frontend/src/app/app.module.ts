c4575f135601be7862f31acce1a65c972d49cb7e
Brandon Lim <b_loong5@hotmail.com>
2018-01-24 12:42:30 -0800

    refactor details display into a separate component

--- frontend/oc6-ng-frontend/src/app/app.module.ts
+++ frontend/oc6-ng-frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PaddlersComponent } from './paddlers/paddlers.component';
+import { PaddlerDetailComponent } from './paddler-detail/paddler-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PaddlersComponent,
+    PaddlerDetailComponent,
   ],
   imports: [
     BrowserModule,
