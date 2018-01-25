f4ea44c28525d2e85270aad66ca9bd70142a395c
Brandon Lim <b_loong5@hotmail.com>
2018-01-25 10:37:15 -0800

    Renamed the mock paddlers and changed a bit of styling

--- frontend/oc6-ng-frontend/src/app/mock-paddlers.ts
+++ frontend/oc6-ng-frontend/src/app/mock-paddlers.ts
@@ -1,14 +1,14 @@
 import { Paddler } from './paddler'
 
 export const PADDLERS: Paddler[] = [
-    { id: 11, name: 'Mr. Nice', paddlingSide: 'both', time: 123 },
-    { id: 12, name: 'Narco', paddlingSide: 'right', time: 22},
-    { id: 13, name: 'Bombasto', paddlingSide: 'left', time: 60 },
-    { id: 14, name: 'Celeritas', paddlingSide: 'left', time: 10000 },
-    { id: 15, name: 'Magneta', paddlingSide: 'right', time: 1000000 },
-    { id: 16, name: 'RubberMan', paddlingSide: 'both', time: 100 },
-    { id: 17, name: 'Dynama', paddlingSide: 'both', time: 20 },
-    { id: 18, name: 'Dr IQ', paddlingSide: 'both', time: 50 },
-    { id: 19, name: 'Magma', paddlingSide: 'both', time: 24601 },
-    { id: 20, name: 'Tornado', paddlingSide: 'both', time: 88889999 },
+    { id: 11, name: 'Nodnarb', paddlingSide: 'both', time: 123 },
+    { id: 12, name: 'Cherry', paddlingSide: 'right', time: 22},
+    { id: 13, name: 'Ryan', paddlingSide: 'left', time: 60 },
+    { id: 14, name: 'Alex', paddlingSide: 'left', time: 10000 },
+    { id: 15, name: 'Juju', paddlingSide: 'right', time: 1000000 },
+    { id: 16, name: 'Elizabeth', paddlingSide: 'both', time: 100 },
+    { id: 17, name: 'Yuyu', paddlingSide: 'both', time: 20 },
+    { id: 18, name: '5km biceps', paddlingSide: 'both', time: 50 },
+    { id: 19, name: 'Water god', paddlingSide: 'both', time: 24601 },
+    { id: 20, name: 'Carman', paddlingSide: 'both', time: 88889999 },
 ]
