56b08f3e8de7154b2e0509e717286987346d044c
BrandonBlimp <brandonloong.lim@gmail.com>
2018-08-21 17:35:42 -0700

    update gitignore, remove erroneously committed virtual environment folder

--- oc6-venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\brandon\documents\workspace\oc6-rostermaker\oc6-venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
