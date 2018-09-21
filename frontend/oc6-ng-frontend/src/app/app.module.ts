6aa7d40cbbb14759c19bd80042f7ade4e2cc905e
BrandonBlimp <brandonloong.lim@gmail.com>
2018-09-20 19:16:38 -0700

    putting frontend on hold to do backend/db stuff

--- frontend/oc6-ng-frontend/src/app/app.module.ts
+++ frontend/oc6-ng-frontend/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { PaddlersComponent } from './paddlers/paddlers.component';
 import { BoatsComponent } from './boats/boats.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 
 @NgModule({
@@ -22,7 +25,15 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // TODO: Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [ PaddlerService, MessageService ],
   bootstrap: [AppComponent]
--- frontend/oc6-ng-frontend/src/app/paddler.service.ts
+++ frontend/oc6-ng-frontend/src/app/paddler.service.ts
@@ -4,14 +4,22 @@ import { of } from 'rxjs/observable/of';
 import { Paddler } from './paddler';
 import { PADDLERS } from './mock-paddlers';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class PaddlerService {
 
-  constructor(private messageService:MessageService) { }
+  constructor( private messageService: MessageService, private http: HttpClient) { }
 
+  private paddlersUrl = 'api/paddlers';
+
+  // private log(message: string) {
+  //   this.messageService.add('PaddlerService: ${message}');
+  // }
+
+  /** GET paddlers from the server */
   getPaddlers(): Observable<Paddler[]> {
-    return of(PADDLERS);
+    return this.http.get<Paddler[]>(this.paddlersUrl)
   }
 
 }
